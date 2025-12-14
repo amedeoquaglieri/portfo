1cd025d1a4d9a8bbd354ae585fafe2d8b486b9d9
Amedeo Quaglieri <amedeoquaglieri@gmail.com>
2025-12-14 13:19:58 +0930

    myportfolio website

---
+++ server.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+x = 5
+
+@app.route("/")
+def my_home():
+    print('I am AMEDEO QUAGLIERI')
+    return render_template('index.html')
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    print(x)
+    return render_template(page_name)
+
+# ---------------------------------------
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message]) # we pass these parameters as a list
+
+# ---------------------------------------
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            # print(data)
+            # write_to_file(data)
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong. Try again!'
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if valid_login(request.form['username'],
+#                        request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error = 'Invalid username/password'
+#     # the code below is executed if the request method
+#     # was GET or the credentials were invalid
+#     return render_template('login.html', error=error)
